bd095b7ca6cd7885e84719fa1c638f074769874f
Jan Erik Hanssen <jhanssen@gmail.com>
2020-03-18 03:13:50 -0700

    write'n'stuff

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ struct Concat
 {
     std::string base;
     std::vector<std::string> parts;
+    FILE* file;
 } concat;
 
 int main(int argc, char** argv)
@@ -38,28 +39,61 @@ int main(int argc, char** argv)
         Log(Log::Error) << "no url";
         return 1;
     }
+    if (!args.has<std::string>("output")) {
+        Log(Log::Error) << "no output file name";
+        return 1;
+    }
 
-    const auto url = args.get<std::string>("url");
+    const auto url = args.value<std::string>("url");
+    const auto outputfn = args.value<std::string>("output");
+
+    concat.file = fopen(outputfn.c_str(), "w");
+    if (!concat.file) {
+        Log(Log::Error) << "can't open" << outputfn;
+        return 1;
+    }
 
     std::shared_ptr<event::Loop> loop = event::Loop::create();
 
     const auto base = url.rfind('/');
     if (base == std::string::npos) {
-        Log(Log::error) << "no uri base";
+        Log(Log::Error) << "no uri base";
     }
     concat.base = url.substr(0, base + 1);
     Log(Log::Info) << "uri base" << concat.base;
 
     auto fetch = net::Fetch::create();
-    fetch->fetch(url).then([](std::shared_ptr<buffer::Buffer>&& buffer) -> auto& {
+    std::function<void(uint32_t)> downloadNext;
+
+    downloadNext = [&fetch, &downloadNext, &loop](uint32_t cur) {
+        if (cur >= concat.parts.size()) {
+            // done
+            Log(Log::Info) << "done";
+            fclose(concat.file);
+            loop->exit();
+            return;
+        }
+        const std::string suburi = concat.base + concat.parts[cur];
+        //Log(Log::Info) << "fetching" << suburi;
+        fetch = net::Fetch::create();
+        fetch->fetch(suburi).then([&downloadNext, cur](std::shared_ptr<buffer::Buffer>&& buffer) -> void {
+            Log(Log::Info) << "downloaded" << buffer->size() << (cur + 1) << "/" << concat.parts.size();
+            fwrite(buffer->data(), buffer->size(), 1, concat.file);
+            downloadNext(cur + 1);
+        });
+    };
+
+    fetch->fetch(url).then([&downloadNext](std::shared_ptr<buffer::Buffer>&& buffer) -> void {
         // parse buffer, all lines that doesn't start with a '#' is go
-        const char* data = reinterpret_cast<const char*>(buffer.data());
+        const char* data = reinterpret_cast<const char*>(buffer->data());
         size_t off = 0;
-        const size_t max = buffer.size();
+        const size_t max = buffer->size();
         while (off < max) {
             if (data[off] == '#') {
                 // skip until eol
                 while (off < max && data[off] != '\n')
+                    ++off;
+                if (off < max)
                     ++off;
                 continue;
             }
@@ -68,9 +102,13 @@ int main(int argc, char** argv)
             while (off < max && data[off] != '\n')
                 ++off;
             auto suburi = std::string(data + start, off - start);
-            Log(Log::Info) << "parsed sub uri" << suburi;
+            Log(Log::Info) << "parsed sub uri" << suburi << (off - start);
             concat.parts.push_back(std::move(suburi));
+            if (off < max)
+                ++off;
         }
+
+        downloadNext(0);
     });
 
     loop->execute();
